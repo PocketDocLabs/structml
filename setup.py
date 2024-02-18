08665272a436bddf2d920f45c9d31f6419605fb5
PocketDocLabs <visuallyadequate@gmail.com>
2024-02-18 14:33:10 -0800

    modified:   pyproject.toml 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='structml',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
 description='A package for working with data using NLP and ML.',
     author='PocketDoc Labs',
